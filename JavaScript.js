1be65274e1db63ef098d8a0e972135ed82420ec0
Guilherme Alves <guialvest@gmail.com>
2022-02-03 10:47:02 -0300

    Adição de animação e gifs nas cartas

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,7 @@
 let numeroCartas = 0, carta = [], cartas ="";
-let tabuleiro = document.querySelector("section");
+let tabuleiro = document.querySelector(".tabuleiro");
+let gifs = [0,1,2,3,4,5,6,7], contadorGifs=0;
+
 
 perguntaCartas();
 function perguntaCartas(){
@@ -8,13 +10,27 @@ function perguntaCartas(){
     }
     criandoCartas();
 }
+
+function focando(focado){
+    focado.classList.add("focus");
+}
+
 function criandoCartas(){
     for(let i=0; i < numeroCartas; i++){
+        if(contadorGifs===numeroCartas/2){
+            contadorGifs=0;
+        }
         carta[i] = ` 
-        <div>
-        <img src="imagens/front.png" alt="Costas da carta">
+        <div onclick="focando(this)" class="card">
+            <div class="frontFace face">
+                <img src="imagens/front.png" alt="Costas da carta">
+            </div>
+            <div class="backFace face">
+                <img src="imagens/${gifs[contadorGifs]}.gif" alt="Costas da carta">
+            </div>
         </div>
         `
+        contadorGifs++;
     }
     cartas = carta.join(' ')
 }
