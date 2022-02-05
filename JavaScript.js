cea61e0f78715a64c90e5416b6f2e4ad54692ca4
Guilherme Alves <guialvest@gmail.com>
2022-02-05 19:04:43 -0300

    Adição de feedbacks ao usuário, ajuste de layout para mobile

--- JavaScript.js
+++ JavaScript.js
@@ -1,12 +1,13 @@
-let numeroCartas = 0, carta = [], cartas ="";
-let tabuleiro = document.querySelector(".tabuleiro");
+let numeroCartas, carta = [], cartas ="";
+const tabuleiro = document.querySelector(".tabuleiro");
 let gifs = [0,1,2,3,4,5,6], contadorGifs=0,primeiroNumero, segundoNumero, primeiraCarta, segundaCarta, digaNaoProGlitch = true;
+let jogadas=0, tempo=0, acertos =0, intervalo=null;
 
 
-perguntaCartas();
-function perguntaCartas(){
+perguntarCartas();
+function perguntarCartas(){
     while(numeroCartas<4 || numeroCartas>14 || numeroCartas%2!==0){
-        numeroCartas = parseInt(prompt("Escolha um número par de cartas entre 2 e 14")) 
+        numeroCartas = parseInt(prompt("Escolha um número par de cartas entre 4 e 14")) 
     }
     criandoCartas();
 }
@@ -18,19 +19,20 @@ function criandoCartas(){
             contadorGifs=0;
         }
         carta[i] = ` 
-        <div onclick="focandoNaCarta(this, ${gifs[contadorGifs]})" class="card">
-            <div class="frontFace face">
+        <article onclick="focandoNaCarta(this, ${gifs[contadorGifs]})" class="carta">
+            <div class="faceDaFrente face">
                 <img src="imagens/front.png" alt="Costas da carta">
             </div>
-            <div class="backFace face">
+            <div class="faceDeTras face">
                 <img src="imagens/${gifs[contadorGifs]}.gif" alt="Costas da carta">
             </div>
-        </div>
+        </article>
         `
         contadorGifs++;
     }
     carta.sort(embaralhandoCartas);
     cartas = carta.join(' ')
+    tabuleiro.innerHTML= cartas;
 }
 
 function embaralhandoCartas() { 
@@ -38,27 +40,40 @@ function embaralhandoCartas() {
 }
 
 function focandoNaCarta(cartaFocada, numeracaoDaCarta){
+    if(tempo ===0){
+        tempo++;
+        intervalo =setInterval(atualizarCronometro,1000);
+    }
     if(digaNaoProGlitch == true){
         cartaFocada.classList.add("focus");
         if(primeiroNumero == undefined){
             primeiroNumero = numeracaoDaCarta;
             primeiraCarta = cartaFocada;
+            calculandoJogadas();
         }
         else{
             segundoNumero = numeracaoDaCarta;
             segundaCarta = cartaFocada;
             if(primeiraCarta === segundaCarta){
-                segundaCarta=null;
-                segundoNumero=null;
-            }
-            if(primeiroNumero!==segundoNumero){
-                digaNaoProGlitch=false;
-                setTimeout(desvirandoCartas, 900);
-                setTimeout(evitarBug, 900);
-                primeiroNumero = undefined;
+                segundaCarta=undefined;
+                segundoNumero=undefined;
             }
             else{
-                primeiroNumero = undefined;    
+                calculandoJogadas();
+                if(primeiroNumero!==segundoNumero){
+                    digaNaoProGlitch=false;
+                    setTimeout(desvirandoCartas, 1000);
+                    setTimeout(evitarBug, 1000);
+                    primeiroNumero = undefined;
+                }
+                else{
+                    primeiroNumero = undefined;
+                    acertos++;
+                    if(acertos>=numeroCartas/2){
+                    clearInterval(intervalo);
+                    setTimeout(finalizarJogo, 500);
+                    }    
+                }
             }
         }
     }
@@ -73,5 +88,26 @@ function desvirandoCartas(){
 function evitarBug(){
     digaNaoProGlitch=true;
 }
-
-tabuleiro.innerHTML= cartas;
+function calculandoJogadas(){
+    jogadas++;
+    let jogadasFeitas = document.querySelector(".jogadas");
+    jogadasFeitas.innerHTML=`Jogadas feitas: ${jogadas}`
+}
+function atualizarCronometro(){
+    let cronometro = document.querySelector(".cronometro");
+    cronometro.innerHTML=`Tempo: ${tempo}`;
+    tempo++;
+}
+function finalizarJogo(){
+    numeroCartas=undefined;
+    primeiroNumero=undefined, segundoNumero=undefined, primeiraCarta=undefined; segundaCarta=undefined;
+    alert(`Você ganhou em ${jogadas} jogadas / ${tempo} segundos!`);
+    let jogarNovamente = prompt("Deseja jogar novamente ? \n digite 'sim' ou 'não'")
+    if (jogarNovamente === "sim"){
+        jogadas=-1; tempo=0; acertos =0, contadorGifs=0, carta = [], cartas ="";
+        calculandoJogadas()
+        atualizarCronometro()
+        tempo--;
+        perguntarCartas();
+    }
+}
